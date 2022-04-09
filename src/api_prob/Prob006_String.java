e0f07bab29f4c3392cecd641deedcea8801b5b6a
nopainnomoney <lhy1031hy@gmail.com>
2022-04-09 23:59:13 +0900

    API practice

---
+++ src/api_prob/Prob006_String.java
@@ -0,0 +1,48 @@
+package src.api_prob;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+/*
+ * [실행결과]
+ * 97에서 122까지정수 입력:122
+ * z
+ */
+public class Prob006_String {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		while (true) {
+			System.out.print("97에서 122까지정수 입력:");
+			try {
+				int x = sc.nextInt();
+				if (x < 97 || x > 122) {
+					System.out.println("97에서 122까지정수만 입력해주세요.");
+					continue;
+				}
+				System.out.printf("%s\n", result(toChar(x)));
+				break;
+
+			} catch (InputMismatchException e) {
+				System.out.println("숫자만 입력해주세요");
+				sc.nextLine();
+				continue;
+			}
+
+		}
+
+		sc.close();
+
+	}
+
+	public static char toChar(int x) {
+		char c = (char) x;
+		return c;
+	}
+
+	public static String result(char data) {
+		String s = String.valueOf(data);
+		return s;
+	}
+}
