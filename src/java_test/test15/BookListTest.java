7249c8769939aaffdabdf5cdcf8d60ab665ae640
nopainnomoney <lhy1031hy@gmail.com>
2022-04-22 20:35:59 +0900

    find out more effective way

--- src/java_test/test15/BookListTest.java
+++ src/java_test/test15/BookListTest.java
@@ -5,13 +5,9 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.NotSerializableException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,14 +18,12 @@ public class BookListTest {
 		BookListTest test5 = new BookListTest();
 		ArrayList<Book> list = new ArrayList<Book>();
 
-		test5.storeList(list);// Book 객체를 3개 생성하여 리스트에 넣는다.
+		test5.storeList(list);
 
-		test5.saveFile(list); // books.dat 파일에 리스트에 저장된 Book 객체들을 저장한다.
+		test5.saveFile(list);
 		List<Book> booksList = test5.loadFile();
-		// books.dat 파일로부터 객체들을 읽어서 리스트에 담는다.
-		test5.printList(booksList); // 리스트에 저장된 객체 정보를 출력한다.
-		// 할인된 가격은 booksList 에 기록된 객체 정보를 사용하여 getter 로 계산 출력한다.
 
+		test5.printList(booksList);
 	}
 
 	public void storeList(List<Book> list) {
@@ -45,15 +39,17 @@ public class BookListTest {
 
 	public void saveFile(List<Book> list) {
 
-		FileOutputStream fos = null;
-		ObjectOutputStream oos = null;
-		try {
-			fos = new FileOutputStream(file);
-			oos = new ObjectOutputStream(fos);
-
-			for (Book b : list) {
-				oos.writeObject(b);
-			}
+		
+		try(ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(file))) {
+			list.forEach(t -> {
+				try {
+					oos.writeObject(t);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			});
+		
 
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
@@ -62,56 +58,43 @@ public class BookListTest {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 
-		} finally {
-			try {
-				oos.close();
-				fos.close();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+			
 		}
 	}
 
 	public List<Book> loadFile() {
 
 		ArrayList<Book> list = null;
-		ObjectInputStream ois = null;
-		try {
-			ois = new ObjectInputStream(new FileInputStream(file));
-			
+		try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(file))) {
+
 			list = new ArrayList<Book>();
-			Object b = null;
-			while ((b = ois.readObject()) != null) {
-			//	System.out.println("?");
-				list.add((Book) b);
+			while (true) {
+				try {
+					list.add((Book) ois.readObject());
+				} catch (EOFException e) {
+					break;
+				}
 
 			}
 
-		
-			ois.close();
-		} catch (EOFException e) {
-
-		
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-			
-	
+
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}
+		} 
 
 		return list;
 
 	}
 
 	public void printList(List<Book> list) {
-		list.forEach(e -> System.out.println(e));
+		list.forEach(System.out::println);
 	}
 
 }
